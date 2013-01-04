be793c70fb07ce2e840e7e3b41429383c347f8ae
Um Nontasuwan <nitipit@gmail.com>
2013-01-05 00:46:27 +0700

    Fix python PEP8

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ import os
 data = list()
 for d in os.walk('markdo/'):
     if len(d[2]) > 0:
-        path_list = map(lambda x: str.join('/',os.path.join(d[0],x).split('/')[1:]), d[2])
+        path_list = map(
+            lambda x: str.join('/', os.path.join(d[0], x).split('/')[1:]),
+            d[2]
+        )
         data.extend(path_list)
 
 setup(
-    name = "MarkDo",
-    version = "0.1",
-    package_dir = {'markdo': 'markdo'},
-    packages = ['markdo'],
-    scripts = ['markdo/markdo'],
-    package_data = {'markdo': data}
+    name="MarkDo",
+    version="0.1",
+    package_dir={'markdo': 'markdo'},
+    packages=['markdo'],
+    scripts=['markdo/markdo'],
+    package_data={'markdo': data},
+    requires=['appkit', ],
 )
